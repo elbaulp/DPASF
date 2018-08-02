7ece69fb3602e90995e875c0f70c235af0871d66
Alejandro Alcalde <algui91@gmail.com>
2018-08-02 17:37:33 +0200

    Added initial code for PID Discretizer

---
+++ dpasf/src/main/scala/com/elbauldelprogramador/discretizers/PIDiscretizerTransformer.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2018  Alejandro Alcalde
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.elbauldelprogramador.discretizers
+
+import org.apache.flink.api.scala._
+import org.apache.flink.ml.common.{ LabeledVector, Parameter, ParameterMap }
+import org.apache.flink.ml.pipeline.{ FitOperation, TransformDataSetOperation, Transformer }
+
+/**
+ * Incremental Discretization Algorithm
+ */
+class PIDiscretizerTransformer extends Transformer[PIDiscretizerTransformer] {
+
+  import PIDiscretizerTransformer._
+
+  // TODO docs
+  def setBins(l2Updates: Int): PIDiscretizerTransformer = {
+    parameters add (L2UpdateExamples, l2Updates)
+    this
+  }
+
+  // TODO docs
+  def setL2Bins(bins: Int): PIDiscretizerTransformer = {
+    parameters add (L1InitialBins, bins)
+    this
+  }
+
+  // TODO docs
+  def setInitElements(n: Int): PIDiscretizerTransformer = {
+    parameters add (InitialElements, n)
+    this
+  }
+
+  // TODO docs
+  def setNumAttr(alpha: Double): PIDiscretizerTransformer = {
+    parameters add (Alpha, alpha)
+    this
+  }
+
+  // TODO docs
+  def setMin(min: Int): PIDiscretizerTransformer = {
+    parameters add (Min, min)
+    this
+  }
+
+  // TODO docs
+  def setMax(max: Int): PIDiscretizerTransformer = {
+    parameters add (Max, max)
+    this
+  }
+}
+
+object PIDiscretizerTransformer {
+
+  // ========================================== Parameters =========================================
+  private[PIDiscretizerTransformer] case object L2UpdateExamples extends Parameter[Int] {
+    val defaultValue: Option[Int] = Some(1000)
+  }
+
+  private[PIDiscretizerTransformer] case object Alpha extends Parameter[Double] {
+    val defaultValue: Option[Double] = Some(.75)
+  }
+
+  private[PIDiscretizerTransformer] case object Min extends Parameter[Int] {
+    val defaultValue: Option[Int] = Some(0)
+  }
+
+  private[PIDiscretizerTransformer] case object Max extends Parameter[Int] {
+    val defaultValue: Option[Int] = Some(1)
+  }
+
+  private[PIDiscretizerTransformer] case object L1InitialBins extends Parameter[Int] {
+    val defaultValue: Option[Int] = Some(200)
+  }
+
+  private[PIDiscretizerTransformer] case object InitialElements extends Parameter[Int] {
+    val defaultValue: Option[Int] = Some(100)
+  }
+
+  // ========================================== Factory methods ====================================
+  def apply(): PIDiscretizerTransformer = new PIDiscretizerTransformer
+
+  // ========================================== Operations =========================================
+
+  // TODO doc
+  implicit val fitNoOp = new FitOperation[PIDiscretizerTransformer, LabeledVector] {
+    override def fit(
+      instance: PIDiscretizerTransformer,
+      fitParameters: ParameterMap,
+      input: DataSet[LabeledVector]): Unit = ()
+  }
+
+  // TODO doc
+  implicit val transformLabeledIDADiscretizer = new TransformDataSetOperation[PIDiscretizerTransformer, LabeledVector, LabeledVector] {
+    override def transformDataSet(
+      instance: PIDiscretizerTransformer,
+      transformParameters: ParameterMap,
+      input: DataSet[LabeledVector]): DataSet[LabeledVector] = {
+      val resultingParameters = instance.parameters ++ transformParameters
+
+      ???
+    }
+  }
+}
